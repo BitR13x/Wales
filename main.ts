9b95d8f3ee8df26cb8d192d833826f421f21ac17
BitR13x <BitR13x@protonmail.com>
2021-12-07 23:38:38 +0100

    better youtube searches

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const ytdl = require('ytdl-core');
+const { getInfo } = require('ytdl-getinfo');
 const client = new Discord.Client();
 const { prefix, token } = require('./config.json');
 
@@ -13,40 +14,33 @@ client.once('ready', () => {
 
 setTimeout(drinking_time,3600)
 function drinking_time() {
-    client.channels.cache.get("911272250106339388").send("Drinking Time") //, {embed: {image: {url: "https://imgur.com/XcGqJ3z",}}})
-    //mess.react('<:blobreach:123456789012345678>');
+    try {
+        client.channels.cache.get("911272250106339388").send("Drinking Time") //, {embed: {image: {url: "https://imgur.com/XcGqJ3z",}}})
+    }//mess.react('<:blobreach:123456789012345678>');
+    finally { return 0;}
 };
 
-function playSong(msg, args, voiceChannel, playing) {
+function playSong(msg, song, voiceChannel, loop=false) {
     if (voiceChannel) {
         voiceChannel.join().then(connection => {
             if (!vol) {
-                var vol = 75 / 100
+                var vol = 50 / 100
             }
 
-            if (args[0]) {
-                console.log(args)
-                playing.push(args[0])
-            }
-            if (args[0] in playing) {
-                console.log("it works")
-            }
-
-            this.dispatcher = connection.play(ytdl(playing[0], { volume: vol, filter : 'audioonly' }));
+            this.dispatcher = connection.play(ytdl(song, { volume: 0.5, filter : 'audioonly', type: 'opus' }));
             
             this.dispatcher.on("start", () => {
-                console.log("audio playing")
-                msg.channel.send(`now playing: ${playing[0]}`);
+                getInfo(song).then(info => {
+                    msg.channel.send(`Now playing: ${info.items[0].title}`);
+                  })
             });
-            
+ 
             this.dispatcher.on("finish", () => {
-                console.log("left channel");
+                if (loop) {
+                    playSong(msg, song, voiceChannel, loop);
+                } 
                 
-                playing.shift()
-                if (playing.length > 0){
-                    playSong(msg, args, voiceChannel, playing)
-
-                } else if (playing.length === 0) {
+                else {
                     voiceChannel.leave();
                     msg.channel.send("Bye, Bye....");
                 }
@@ -57,45 +51,64 @@ function playSong(msg, args, voiceChannel, playing) {
         };
     }
 
-function joinChannel(msg, voiceChannel) {
-    // if joined just play if !joined join and play
-    if (voiceChannel) {
-        voiceChannel.join()
-        msg.channel.send("Joined channel")
-    //connection.play(broadcast);
-    } else {
-        msg.reply("You need to be in a voice channel")
-    }
-}
-
 client.on('message', msg => {
     if (!msg.content.startsWith(prefix) || msg.author.bot) return;
     const args = msg.content.split(" ");
     const command = args.shift().toLowerCase()
     const voiceChannel = msg.member.voice.channel;
-    var playing = []
+//     var playing = []
 
     if (command === prefix + "p") {
         if (!args[0]) return msg.reply("Please provide link")
-        playSong(msg, args, voiceChannel, playing)
+        else if (args[0].startsWith("https://")){
+            playSong(msg, args[0], voiceChannel, loop);
+        } else {
+            msg.channel.send("Searching")
+            getInfo(args.join(" ")).then(info => {
+                playSong(msg, info.items[0].webpage_url, voiceChannel, loop);
+            })
+        }
+    }
+
+    else if (command == prefix + "loop") {
+        if (loop) {
+            var loop = false;
+            msg.channel.send("Loop disabled");
+        }
+        else {
+            var loop = true;
+            msg.channel.send("Loop enabled");
+        };
+        
     }
 
 
     else if (command === prefix + "leave"){
-        //if not already joined
-//        if (loop) {
-//            var loop = false
-//        }
-        try {
-            voiceChannel.leave()
-            playing.length = 0;
-        }
-        catch {
-            msg.reply("I am not connected")
-        };
+        msg.channel.send("Bye, Bye....");
+        voiceChannel.leave();
+//            playing.length = 0;
     }
 
-    else if (command === prefix + "stop"){
+    else if (command === prefix + "pause") {
+        if (this.dispatcher) {
+            this.dispatcher.pause();
+         } else {
+            msg.channel.send("Nothing is playing");
+         }
+         
+     }
+ 
+     else if (command === prefix + "resume") {
+         if (this.dispatcher) {
+            this.dispatcher.resume();
+         } else {
+            msg.channel.send("Nothing is paused");
+         }
+         
+     }
+ 
+
+/*     else if (command === prefix + "stop"){
         //if something playing [make array (stack)]
         if (playing.length > 0 && this.dispatcher) {
             this.dispatcher.destroy()
@@ -103,16 +116,16 @@ client.on('message', msg => {
             msg.reply("Nothing playing")
         };
     }
-
+ */
     else if (command === prefix + "volume") {
         if (isNaN(args[0])) {
-            msg.reply("You need enter number")
+            msg.reply("You need enter number");
         }
         else if (args[0]) {
-            var vol = Number(args[0]) / 100
-            msg.channel.send("Volume successfully changed, default 75")
+            var vol = Number(args[0]) / 100;
+            msg.channel.send("Volume successfully changed, default 75");
         } else {
-            msg.reply("You need enter single number")
+            msg.reply("You need enter single number");
         };
     }
 
@@ -133,8 +146,8 @@ client.on('message', msg => {
 
         if (taggedUser) {
             if (args[1]) {
-                if (isNaN(args[1])) return msg.reply("You need to use number")
-                else if (Number(args[1]) > 20) return msg.reply("More than 20 is not allowed")
+                if (isNaN(args[1])) return msg.reply("You need to use number");
+                else if (Number(args[1]) > 20) return msg.reply("More than 20 is not allowed");
 
                 else {
                     var count = Number(args[1])
@@ -143,10 +156,10 @@ client.on('message', msg => {
             else if (args[2]) {
                 if (isNaN(args[2])) return msg.reply("You need to use number")
 
-                else if (Number(args[2]) > 10) return msg.reply("More than 10 is not allowed")
+                else if (Number(args[2]) > 10) return msg.reply("More than 10 is not allowed");
 
                 else {
-                    var count = Number(args[2])
+                    var count = Number(args[2]);
                 };
             }
 
@@ -154,14 +167,13 @@ client.on('message', msg => {
                 msg.channel.send(`Get Your Ass Over Here: <@${taggedUser.id}>`)
             };
         } else {
-            msg.reply("You need tag someone")
+            msg.reply("You need tag someone");
         };
      }
 
 })
 
 client.login(token);
-// https://stackoverflow.com/questions/47045805/playing-an-audio-file-using-discord-js-and-ytdl-core
 
 
       /* meme post
